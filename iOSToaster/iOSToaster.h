bb9cfb1def4f36177cb15bae5e7fc9c48b4ce11a
enescetinkaya <enes.cetinkaya@yahoo.com>
2022-12-17 21:41:19 +0300

    adding files

---
+++ iOSToaster/iOSToaster.h
@@ -0,0 +1,18 @@
+//
+//  iOSToaster.h
+//  iOSToaster
+//
+//  Created by enes cetinkaya on 17.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSToaster.
+FOUNDATION_EXPORT double iOSToasterVersionNumber;
+
+//! Project version string for iOSToaster.
+FOUNDATION_EXPORT const unsigned char iOSToasterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSToaster/PublicHeader.h>
+
+
